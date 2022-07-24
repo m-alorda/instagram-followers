bcb7ad91056573a216abbbccbb4294f5e5d7b78e
m-alorda <miguel.alorda.cantero@gmail.com>
2022-07-24 19:35:26 +0200

    Refactored instagramFollowers.js

--- instagramFollowers.js
+++ instagramFollowers.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-const get_user_id = async (username) => {
+const fetch_user_id = async (username) => {
   console.log(`Fetching user id for @${username}`);
   return fetch(
     `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`,
@@ -41,7 +41,55 @@ const get_user_id = async (username) => {
     });
 };
 
-const find_not_followed_and_following_back = async (username) => {
+const fetch_not_followed_and_following_back = async (username) => {
+  const fetch_usernames = async (
+    user_id,
+    endpoint_hash,
+    user_data_path,
+    after
+  ) => {
+    const url_params = {
+      id: user_id,
+      include_reel: true,
+      fetch_mutual: true,
+      first: 50, // Maximum is 50
+      after: after === undefined ? null : after,
+    };
+    const request_url =
+      "https://www.instagram.com/graphql/query/" +
+      `?query_hash=${endpoint_hash}` +
+      `&variables=${encodeURIComponent(JSON.stringify(url_params))}`;
+    console.log(
+      `Fetching ${user_data_path} of user ${user_id} from endpoint ${endpoint_hash}`
+    );
+    return fetch(request_url)
+      .then((res) => res.json())
+      .then((res) => res.data.user[user_data_path])
+      .then((data) => {
+        return {
+          has_next: data.page_info.has_next_page,
+          after: data.page_info.end_cursor,
+          fetched_usernames: data.edges.map((element) => element.node.username),
+        };
+      });
+  };
+  const fetch_all_usernames = async (list_config, user_id) => {
+    const result_list = [];
+    let after = null;
+    let has_next = true;
+    while (has_next) {
+      const result = await fetch_usernames(
+        user_id,
+        list_config.hash,
+        list_config.path,
+        after
+      );
+      has_next = result.has_next;
+      after = result.after;
+      result_list.push(...result.fetched_usernames);
+    }
+    return result_list;
+  };
   const config = {
     followers: {
       hash: "c76146de99bb02f6415203be841dd25a",
@@ -52,43 +100,12 @@ const find_not_followed_and_following_back = async (username) => {
       path: "edge_follow",
     },
   };
-  const get_list = async (list_config, user_id) => {
-    const result_list = [];
-    let after = null;
-    let has_next = true;
-    const url_params = {
-      id: user_id,
-      include_reel: true,
-      fetch_mutual: true,
-      first: 50, // Maximum is 50
-    };
-    while (has_next) {
-      url_params.after = after;
-      const request_url =
-        "https://www.instagram.com/graphql/query/" +
-        `?query_hash=${list_config.hash}` +
-        `&variables=${encodeURIComponent(JSON.stringify(url_params))}`;
-      console.log("Fetching data");
-      await fetch(request_url)
-        .then((res) => res.json())
-        .then((res) => res.data.user[list_config.path])
-        .then((data) => {
-          has_next = data.page_info.has_next_page;
-          after = data.page_info.end_cursor;
-          const recv_usernames = data.edges.map(
-            (element) => element.node.username
-          );
-          result_list.push(...recv_usernames);
-        });
-    }
-    return result_list;
-  };
 
-  const user_id = await get_user_id(username);
+  const user_id = await fetch_user_id(username);
   console.log("Requesting followers");
-  const followers_task = get_list(config.followers, user_id);
+  const followers_task = fetch_all_usernames(config.followers, user_id);
   console.log("Requesting following");
-  const following_task = get_list(config.following, user_id);
+  const following_task = fetch_all_usernames(config.following, user_id);
 
   const followers = await followers_task;
   const following = await following_task;
@@ -108,7 +125,7 @@ const find_not_followed_and_following_back = async (username) => {
 (async () => {
   const prompt_valid_username = async () => {
     let username = prompt("Please, enter your username");
-    while ((await get_user_id(username)) == null) {
+    while ((await fetch_user_id(username)) == null) {
       username = prompt(
         `The given username was not found (@${username}). Please, enter another one`
       );
@@ -117,7 +134,7 @@ const find_not_followed_and_following_back = async (username) => {
   };
 
   const username = await prompt_valid_username();
-  const data = await find_not_followed_and_following_back(username);
+  const data = await fetch_not_followed_and_following_back(username);
   console.log(
     `Not followed back (${data.not_followed_back.length}): ${data.not_followed_back}`
   );
